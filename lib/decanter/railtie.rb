ad985763ce4f9a8f235608e3dda2198a9140dee4
David Corwin <dave@launchpadlab.com>
2016-02-14 14:49:13 -0600

    fix initializer

--- lib/decanter/railtie.rb
+++ lib/decanter/railtie.rb
@@ -3,7 +3,7 @@ require_relative 'extensions'
 
 class Decanter::Railtie < Rails::Railtie
 
-  initializer 'decanter.configure'
+  initializer 'decanter.configure' do
     ActiveRecord::Base.include(Decanter::Extensions) if defined? ActiveRecord
   end
 end
