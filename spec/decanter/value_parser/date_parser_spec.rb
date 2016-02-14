7f17ab787057cbe9929d0fd6b3c98ae08aea1387
David Corwin <dave@launchpadlab.com>
2016-02-13 22:06:05 -0600

    add date parser and full active support

--- spec/decanter/value_parser/date_parser_spec.rb
+++ spec/decanter/value_parser/date_parser_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe 'DateParser' do
   describe '#parse' do
-    it 'returns a date' do
-      expect(Decanter::ValueParser::DateParser.parse('2/21/1990')).to be_a Date
+    context 'with a valid date string of default form ' do
+      it 'returns a date' do
+        expect(Decanter::ValueParser::DateParser.parse('2/21/1990')).to eq Date.new(2,21,1990)
+      end
     end
   end
 end
