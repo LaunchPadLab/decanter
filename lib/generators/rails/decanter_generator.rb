5649e8c11f99cc317dd717fe8520a5e7f594643e
Ryan Francis <ryan.p.francis@gmail.com>
2016-02-15 13:06:10 -0600

    added decanters generators with ability to pass inputs and associations

--- lib/generators/rails/decanter_generator.rb
+++ lib/generators/rails/decanter_generator.rb
@@ -3,24 +3,33 @@ module Rails
     class DecanterGenerator < NamedBase
       source_root File.expand_path('../templates', __FILE__)
       check_class_collision :suffix => 'Decanter'
+      ASSOCIATION_TYPES = [:has_many, :has_one, :belongs_to]
 
       argument :attributes, :type => :array, :default => [], :banner => 'field:type field:type'
 
       class_option :parent, :type => :string, :desc => 'The parent class for the generated decanter'
 
-      def create_serializer_file
-        template 'serializer.rb.erb', File.join('app/decanters', class_path, "#{file_name}_decanter.rb")
+      def create_decanter_file
+        template 'decanter.rb.erb', File.join('app/decanters', class_path, "#{file_name}_decanter.rb")
       end
 
       private
 
-      def attributes_names
-        [:id] + attributes.reject(&:reference?).map! { |a| a.name.to_sym }
+      def inputs
+        attributes.find_all { |attr| ASSOCIATION_TYPES.exclude?(attr.type) }
       end
 
-      def association_names
-        attributes.select(&:reference?).map! { |a| a.name.to_sym }
+      def associations
+        attributes - inputs
       end
+
+      # def attributes_names
+      #   [:id] + attributes.reject(&:reference?).map! { |a| a.name.to_sym }
+      # end
+
+      # def association_names
+      #   attributes.select(&:reference?).map! { |a| a.name.to_sym }
+      # end
 
       def parent_class_name
         'Decanter::Base'
