d29cdfa12350305cdbc80f79644b9b80a575087e
David Corwin <dave@launchpadlab.com>
2016-02-25 13:23:38 -0600

    updating

--- lib/decanter/railtie.rb
+++ lib/decanter/railtie.rb
@@ -9,6 +9,20 @@ class Decanter::Railtie < Rails::Railtie
     end
   end
 
+  # initializer 'decanter.load' do |app|
+  #   ActiveSupport.on_load :decanter do
+  #     Dir[ File.expand_path(Rails.root.join("app/decanter/**/*.rb")) ].each do |file|
+  #       require file
+  #     end
+  #   end
+  # end
+
+  config.to_prepare do
+    Dir[ File.expand_path(Rails.root.join("app/decanter/**/*.rb")) ].each do |file|
+      require file
+    end
+  end
+
   generators do |app|
     Rails::Generators.configure!(app.config.generators)
     Rails::Generators.hidden_namespaces.uniq!
