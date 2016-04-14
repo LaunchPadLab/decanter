4c7cad9f4d77c60f5e3410d74798f663ccb0a9fc
David Corwin <dave@launchpadlab.com>
2016-04-14 11:14:52 -0500

    reformat

--- lib/decanter/value_parser.rb
+++ lib/decanter/value_parser.rb
@@ -11,9 +11,9 @@ module Decanter
         else
           raise ArgumentError.new("cannot lookup parser for #{klass_or_sym} with class #{klass_or_sym.class}")
         end.concat('Parser')
-        parser_str.safe_constantize ||
-        ("Decanter::ValueParser::" + parser_str).safe_constantize ||
-        raise ArgumentError.new("cannot find parser #{parser_str}")
+
+        parser = parser_str.safe_constantize || ("Decanter::ValueParser::" + parser_str).safe_constantize
+        raise ArgumentError.new("cannot find parser #{parser_str}") unless parser
       end
     end
   end
