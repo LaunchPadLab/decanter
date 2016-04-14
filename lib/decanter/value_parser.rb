32f6a38a251443065c45442eb7173edc4f70b1fb
David Corwin <dave@launchpadlab.com>
2016-04-14 11:12:44 -0500

    look in Decanter::ValueParser

--- lib/decanter/value_parser.rb
+++ lib/decanter/value_parser.rb
@@ -3,14 +3,17 @@ module Decanter
 
     class << self
       def parser_for(klass_or_sym)
-        case klass_or_sym
+        parser_str = case klass_or_sym
         when Class
           klass_or_sym.name
         when Symbol
           klass_or_sym.to_s.singularize.camelize
         else
           raise ArgumentError.new("cannot lookup parser for #{klass_or_sym} with class #{klass_or_sym.class}")
-        end.concat('Parser').constantize
+        end.concat('Parser')
+        parser_str.safe_constantize ||
+        ("Decanter::ValueParser::" + parser_str).safe_constantize ||
+        raise ArgumentError.new("cannot find parser #{parser_str}")
       end
     end
   end
