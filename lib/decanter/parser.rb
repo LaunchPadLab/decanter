baa97cae457bf6f64bd351873a200a81a29266c2
mrjonesbot <nate@mrjones.io>
2017-11-30 17:41:23 -0600

    Bump to 1.0.2

--- lib/decanter/parser.rb
+++ lib/decanter/parser.rb
@@ -1,6 +1,10 @@
+require_relative 'parser/utils'
+
 module Decanter
   module Parser
     class << self
+      include Utils
+
       def parsers_for(klass_or_syms)
         Array.wrap(klass_or_syms)
              .map { |klass_or_sym| klass_or_sym_to_str(klass_or_sym) }
@@ -27,32 +31,6 @@ module Decanter
         parser_str.safe_constantize ||
           concat_str(parser_str).safe_constantize ||
           raise(NameError.new("cannot find parser #{parser_str}"))
-      end
-
-      # extract string transformation strategies
-      def symbol_to_string(klass_or_sym)
-        if singular_class_present?(klass_or_sym)
-          singularize_and_camelize_str(klass_or_sym)
-        else
-          camelize_str(klass_or_sym)
-        end
-      end
-
-      def singular_class_present?(klass_or_sym)
-        parser_str = singularize_and_camelize_str(klass_or_sym)
-        concat_str(parser_str).safe_constantize.present?
-      end
-
-      def singularize_and_camelize_str(klass_or_sym)
-        klass_or_sym.to_s.singularize.camelize
-      end
-
-      def camelize_str(klass_or_sym)
-        klass_or_sym.to_s.camelize
-      end
-
-      def concat_str(parser_str)
-        "Decanter::Parser::".concat(parser_str)
       end
 
       # expand to include preparsers
---
+++ lib/decanter/parser/utils.rb
@@ -0,0 +1,31 @@
+module Decanter
+  module Parser
+    module Utils
+      # extract string transformation strategies
+      def symbol_to_string(klass_or_sym)
+        if singular_class_present?(klass_or_sym)
+          singularize_and_camelize_str(klass_or_sym)
+        else
+          camelize_str(klass_or_sym)
+        end
+      end
+
+      def singular_class_present?(klass_or_sym)
+        parser_str = singularize_and_camelize_str(klass_or_sym)
+        concat_str(parser_str).safe_constantize.present?
+      end
+
+      def singularize_and_camelize_str(klass_or_sym)
+        klass_or_sym.to_s.singularize.camelize
+      end
+
+      def camelize_str(klass_or_sym)
+        klass_or_sym.to_s.camelize
+      end
+
+      def concat_str(parser_str)
+        'Decanter::Parser::'.concat(parser_str)
+      end
+    end
+  end
+end
