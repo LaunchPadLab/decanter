6b47eab8bb232545ca3e24fc9fafe2e3c457fbf9
Patrick J Cherry <patrick@bytemark.co.uk>
2018-10-17 15:13:29 +0100

    Remove model-stomping

--- lib/decanter/railtie.rb
+++ lib/decanter/railtie.rb
@@ -2,21 +2,16 @@
 
 require 'decanter'
 
-class Decanter::Railtie < Rails::Railtie
-  initializer 'decanter.active_record' do
-    ActiveSupport.on_load :active_record do
-      require 'decanter/extensions'
-      Decanter::Extensions::ActiveRecordExtensions.enable!
+module Decanter
+  class Railtie < Rails::Railtie
+    initializer 'decanter.parser.autoload', before: :set_autoload_paths do |app|
+      app.config.autoload_paths << Rails.root.join('lib/decanter/parsers')
     end
-  end
 
-  initializer 'decanter.parser.autoload', before: :set_autoload_paths do |app|
-    app.config.autoload_paths << Rails.root.join('lib/decanter/parsers')
-  end
-
-  generators do |app|
-    Rails::Generators.configure!(app.config.generators)
-    Rails::Generators.hidden_namespaces.uniq!
-    require 'generators/rails/resource_override'
+    generators do |app|
+      Rails::Generators.configure!(app.config.generators)
+      Rails::Generators.hidden_namespaces.uniq!
+      require 'generators/rails/resource_override'
+    end
   end
 end
