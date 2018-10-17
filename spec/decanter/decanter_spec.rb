1c47f1b1b00e668187e47b44f48a987eedf42bc9
Patrick J Cherry <patrick@bytemark.co.uk>
2018-10-17 19:31:43 +0100

    Improved legibility of the Decanter test

--- spec/decanter/decanter_spec.rb
+++ spec/decanter/decanter_spec.rb
@@ -3,92 +3,92 @@
 require 'spec_helper'
 
 describe Decanter do
-  DecanterDecanter = Class.new(Decanter::Base) do
+  DecanterSpecDecanter = Class.new(Decanter::Base) do
     def self.name
-      'DecanterDecanter'
+      'DecanterSpecDecanter'
     end
   end
 
   describe '#decanter_from' do
     context 'for a string' do
       context 'when a corresponding decanter exists' do
-        let(:decanter) { 'DecanterDecanter' }
+        let(:the_decanter) { 'DecanterSpecDecanter' }
 
         it 'returns the decanter' do
-          expect(Decanter.decanter_from(decanter)).to eq DecanterDecanter
+          expect(Decanter.decanter_from(the_decanter)).to eq DecanterSpecDecanter
         end
       end
 
       context 'when a corresponding class does not exist' do
-        let(:decanter) { 'DecanterbarDecanter' }
+        let(:the_decanter) { 'DecanterbarDecanter' }
 
         it 'raises a name error' do
-          expect { Decanter.decanter_from(decanter) }
-            .to raise_error(NameError, "uninitialized constant #{decanter}")
+          expect { Decanter.decanter_from(the_decanter) }
+            .to raise_error(NameError, "uninitialized constant #{the_decanter}")
         end
       end
 
       context 'when a corresponding class exists but it is not a decanter' do
-        let(:decanter) { String }
+        let(:the_decanter) { String }
 
         it 'raises an argument error' do
-          expect { Decanter.decanter_from(decanter) }
-            .to raise_error(ArgumentError, "#{decanter.name} is not a decanter")
+          expect { Decanter.decanter_from(the_decanter) }
+            .to raise_error(ArgumentError, "#{the_decanter.name} is not a decanter")
         end
       end
     end
 
     context 'for a class' do
       context 'when a corresponding decanter exists' do
-        let(:decanter) { DecanterDecanter }
+        let(:the_decanter) { DecanterSpecDecanter }
 
         it 'returns the decanter' do
-          expect(Decanter.decanter_from(decanter)).to eq DecanterDecanter
+          expect(Decanter.decanter_from(the_decanter)).to eq DecanterSpecDecanter
         end
       end
 
       context 'when a corresponding class does not exist' do
-        let(:decanter) { Class.new }
+        let(:the_decanter) { Class.new }
 
         it 'raises a name error' do
-          expect { Decanter.decanter_from(decanter) }
-            .to raise_error(ArgumentError, "#{decanter.name} is not a decanter")
+          expect { Decanter.decanter_from(the_decanter) }
+            .to raise_error(ArgumentError, "#{the_decanter.name} is not a decanter")
         end
       end
 
       context 'when a corresponding class exists but it is not a decanter' do
-        let(:decanter) { String }
+        let(:the_decanter) { String }
 
         it 'raises an argument error' do
-          expect { Decanter.decanter_from(decanter) }
-            .to raise_error(ArgumentError, "#{decanter.name} is not a decanter")
+          expect { Decanter.decanter_from(the_decanter) }
+            .to raise_error(ArgumentError, "#{the_decanter.name} is not a decanter")
         end
       end
     end
 
     context 'for a symbol' do
-      let(:decanter) { :decanter }
+      let(:the_decanter) { :decanter_spec }
 
       it 'raises an argument error' do
-        expect { Decanter.decanter_from(decanter) }
-          .to raise_error(ArgumentError, "cannot find decanter from #{decanter} with class #{decanter.class}")
+        expect { Decanter.decanter_from(the_decanter) }
+          .to raise_error(ArgumentError, "cannot find decanter from #{the_decanter} with class #{the_decanter.class}")
       end
     end
   end
 
   describe '#decanter_for' do
     context 'for a string' do
-      let(:decanter) { 'Decanter' }
+      let(:the_decanter) { 'DecanterSpec' }
 
       it 'raises an argument error' do
-        expect { Decanter.decanter_for(decanter) }
-          .to raise_error(ArgumentError, "cannot lookup decanter for #{decanter} with class #{decanter.class}")
+        expect { Decanter.decanter_for(the_decanter) }
+          .to raise_error(ArgumentError, "cannot lookup decanter for #{the_decanter} with class #{the_decanter.class}")
       end
     end
 
     context 'for a class' do
       context 'when a corresponding decanter does not exist' do
-        let(:decanter) do
+        let(:the_decanter) do
           Class.new do
             def self.name
               'Decanterbar'
@@ -97,41 +97,41 @@ describe Decanter do
         end
 
         it 'raises a name error' do
-          expect { Decanter.decanter_for(decanter) }
-            .to raise_error(NameError, "uninitialized constant #{decanter.name}Decanter")
+          expect { Decanter.decanter_for(the_decanter) }
+            .to raise_error(NameError, "uninitialized constant #{the_decanter.name}Decanter")
         end
       end
 
       context 'when a corresponding decanter exists' do
-        let(:decanter) do
+        let(:the_decanter) do
           Class.new do
             def self.name
-              'Decanter'
+              'DecanterSpec'
             end
           end
         end
 
         it 'returns the decanter' do
-          expect(Decanter.decanter_for(decanter)).to eq DecanterDecanter
+          expect(Decanter.decanter_for(the_decanter)).to eq DecanterSpecDecanter
         end
       end
     end
 
     context 'for a symbol' do
-      let(:decanter) { :foobar }
+      let(:the_decanter) { :foobar }
 
       context 'when a corresponding decanter does not exist' do
         it 'raises a name error' do
-          expect { Decanter.decanter_for(decanter) }
-            .to raise_error(NameError, "uninitialized constant #{decanter.to_s.capitalize.concat('Decanter')}")
+          expect { Decanter.decanter_for(the_decanter) }
+            .to raise_error(NameError, "uninitialized constant #{the_decanter.to_s.capitalize.concat('Decanter')}")
         end
       end
 
       context 'when a corresponding decanter exists' do
-        let(:decanter) { :decanter }
+        let(:the_decanter) { :decanter_spec }
 
         it 'returns the decanter' do
-          expect(Decanter.decanter_for(decanter)).to eq DecanterDecanter
+          expect(Decanter.decanter_for(the_decanter)).to eq DecanterSpecDecanter
         end
       end
     end
