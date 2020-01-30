8be4a6f4962750d65b2fbe27761e53c0434f1bc1
David Pickart <davidstoreypickart@gmail.com>
2020-01-29 21:39:00 -0600

    Amend

--- spec/decanter/parser/phone_parser_spec.rb
+++ spec/decanter/parser/phone_parser_spec.rb
@@ -9,7 +9,8 @@ describe 'PhoneParser' do
   describe '#parse' do
     it 'strips all non-numbers from value and returns a string' do
       expect(parser.parse(:foo, '(12)2-21/19.90')).to match({:foo =>'122211990'})
-    
+    end
+
     context 'with empty string' do
       it 'returns nil' do
         expect(parser.parse(name, '')).to match({name => nil})
