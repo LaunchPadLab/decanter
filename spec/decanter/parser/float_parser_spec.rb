2ffd1e3cc3d655b773c26f8e84845b25f5f754af
diana camacho <dcam1901@gmail.com>
2021-11-16 11:49:08 -0600

    use eq

--- spec/decanter/parser/float_parser_spec.rb
+++ spec/decanter/parser/float_parser_spec.rb
@@ -10,26 +10,26 @@ describe 'FloatParser' do
     context 'with a string' do
       context 'with a positive value' do
         it 'returns a positive float' do
-          expect(parser.parse(name, '1.00')).to match({ foo: 1.00 })
+          expect(parser.parse(name, '1.00')).to eq({ foo: 1.00 })
         end
       end
 
       context 'with a negative value' do
         it 'returns a negative float' do
-          expect(parser.parse(name, '-1.00')).to match({ foo: -1.00 })
+          expect(parser.parse(name, '-1.00')).to eq({ foo: -1.00 })
         end
       end
     end
 
     context 'with empty string' do
       it 'returns nil' do
-        expect(parser.parse(name, '')).to match({ foo: nil })
+        expect(parser.parse(name, '')).to eq({ foo: nil })
       end
     end
 
     context 'with nil' do
       it 'returns nil' do
-        expect(parser.parse(name, nil)).to match({ foo: nil })
+        expect(parser.parse(name, nil)).to eq({ foo: nil })
       end
     end
 
