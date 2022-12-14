f4cd0549f8d721a85ddde0ae7cf071e5061ae60e
Owen Roth <rothowen27@gmail.com>
2022-12-13 18:26:48 -0600

    refactor rescue to still raise an argument error even though it is not an 'invalid date

--- lib/decanter/parser/date_parser.rb
+++ lib/decanter/parser/date_parser.rb
@@ -1,20 +1,21 @@
 module Decanter
   module Parser
     class DateParser < ValueParser
-
       allow Date
 
       parser do |val, options|
-        raise Decanter::ParseError.new 'Expects a single value' if val.is_a? Array
-        next if (val.nil? || val === '')
+        raise Decanter::ParseError, 'Expects a single value' if val.is_a? Array
+        next if val.nil? || val === ''
+
         parse_format = options.fetch(:parse_format, '%m/%d/%Y')
         begin
-        ::Date.strptime(val, parse_format)
+          ::Date.strptime(val, parse_format)
         rescue ArgumentError => e
-          raise Decanter::ValueFormatError.new 'invalid Date value for format' if e.message == "invalid date"
+          raise Decanter::ValueFormatError, 'invalid Date value for format' if e.message == 'invalid date'
+
+          raise ArgumentError, e.message
         end
       end
     end
   end
 end
-
--- lib/decanter/parser/datetime_parser.rb
+++ lib/decanter/parser/datetime_parser.rb
@@ -1,17 +1,19 @@
 module Decanter
   module Parser
     class DateTimeParser < ValueParser
-
       allow DateTime
 
       parser do |val, options|
-        raise Decanter::ParseError.new 'Expects a single value' if val.is_a? Array
-        next if (val.nil? || val === '')
+        raise Decanter::ParseError, 'Expects a single value' if val.is_a? Array
+        next if val.nil? || val === ''
+
         parse_format = options.fetch(:parse_format, '%m/%d/%Y %I:%M:%S %p')
         begin
-        ::DateTime.strptime(val, parse_format)
+          ::DateTime.strptime(val, parse_format)
         rescue ArgumentError => e
-         raise Decanter::ValueFormatError.new 'invalid DateTime value for format' if e.message == "invalid date"
+          raise Decanter::ValueFormatError, 'invalid DateTime value for format' if e.message == 'invalid date'
+
+          raise ArgumentError, e.message
         end
       end
     end
