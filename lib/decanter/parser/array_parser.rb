2a9f417671732e1655820ba9297bf857a79fa10d
Nikki <nikki@launchpadlab.com>
2023-12-01 14:06:25 -0500

    formatting

--- lib/decanter/parser/array_parser.rb
+++ lib/decanter/parser/array_parser.rb
@@ -10,7 +10,7 @@ module Decanter
         # Fetch parser classes for provided keys
         parse_each = options.fetch(:parse_each, :pass)
         item_parsers = Parser.parsers_for(Array.wrap(parse_each))
-        unless item_parsers.all? { |parser| parser <= ValueParser || parser <= PassParser} 
+        unless item_parsers.all? { |parser| parser <= ValueParser || parser <= PassParser } 
           raise Decanter::ParseError.new 'parser(s) for array items must subclass ValueParser'
         end
         # Compose supplied parsers
