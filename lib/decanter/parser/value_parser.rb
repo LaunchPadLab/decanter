d8f7f0ed718354aea812c040ad1cbab47d43b674
Nikki <nikki@launchpadlab.com>
2024-01-11 15:26:33 -0700

    make validate_singularity a private method

--- lib/decanter/parser/value_parser.rb
+++ lib/decanter/parser/value_parser.rb
@@ -9,6 +9,8 @@ module Decanter
         super(name, value, options)
       end
 
+      private
+
       def self.validate_singularity(value)
         raise Decanter::ParseError.new 'Expects a single value' if value.is_a? Array
       end
