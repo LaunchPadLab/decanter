aa6de186d37ffc11fa4eaf689d24a8b4ca98fbba
Owen Roth <69156111+oroth8@users.noreply.github.com>
2025-04-07 10:24:56 -0500

    address another equality operator issue

--- lib/decanter/parser/boolean_parser.rb
+++ lib/decanter/parser/boolean_parser.rb
@@ -6,7 +6,7 @@ module Decanter
       allow TrueClass, FalseClass
 
       parser do |val, _options|
-        next if val.nil? || val === ''
+        next if val.nil? || val == ''
 
         true_values = [1, '1', true, 'true', 'TRUE', 'True']
         true_values.include?(val) || val.to_s.downcase == 'true'
